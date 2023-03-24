a597c9e0c830fb19bdbd80e7cbd87a7b9c8fe716
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-03-24 17:45:02 +0100

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split("");
+    letters = letters.reverse();
+    return letters.join("");
 };
 
 // Do not edit below this line
