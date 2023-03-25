011ea390a22d4c45734ea481333b897a14003f21
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-03-25 11:52:36 +0100

    Finish the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (val of args) {
+        if (array.indexOf(val) != -1) {
+            const index = array.indexOf(val);
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
+
+// A simpler, but more advanced way to do it is to use the 'filter' function,
+// which basically does what we did with the forEach above.
+
+// var removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
+//
 
 // Do not edit below this line
 module.exports = removeFromArray;
