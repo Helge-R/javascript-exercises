7e4c794c762b6369fc252853ace9cb82cef300e9
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-04-06 09:58:39 +0200

    Do the palindrome exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-z]/g, ""); // replace all characters that are not(^) a-z with "". Uses /g to find all matches(global)
+  
+  let stringReverse = string.split("").reverse().join("");
+  return string === stringReverse;
+  
 };
 
 // Do not edit below this line
