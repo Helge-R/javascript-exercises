94d9e02b971c64999b4058bcafcc91c2ad3e52d2
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-04-06 11:35:54 +0200

    Do the "Find the Oldest" exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
 
+    oldestAge = getAge(
+      oldest.yearOfBirth,
+      oldest.yearOfDeath);
+    currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
+
+const getAge = function(birth, death) {
+  if (death == undefined) death = new Date().getFullYear();
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
